a6fd51357fff8ddb99d54ebf7b3f5134c5806821
Thaqif Mikail <oranqif35@gmail.com>
2024-12-03 14:52:15 +0800

    Updated

--- src/components/AddMovieForm.js
+++ src/components/AddMovieForm.js
@@ -3,9 +3,28 @@ import React, { useState } from "react";
 export default function AddMovieForm({ onAddMovie }) {
   const [title, setTitle] = useState("");
 
+  const handleTitle = (e) => {
+    setTitle(e.target.value)
+  }
+
   const handleSubmit = (e) => {
     // TODO: write code to handle form submission
+    e.preventDefault();
+    if (!title) return;
+    const newTitle = {id: Date.now(), title: title, watched: false}
+    onAddMovie(newTitle);
   };
 
-  return; // TODO: add a form to add a new movie
-}
+  return(
+    // TODO: add a form to add a new movie
+    <form onSubmit={handleSubmit}>
+      <input 
+      type="text"
+      placeholder="Movie Title"
+      value={title}
+      onChange={handleTitle}
+      />
+      <button type="submit">Add</button>
+    </form>
+  )
+};
